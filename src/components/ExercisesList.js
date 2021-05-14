6ecd8cf346bbc1cc77a7821f8b85966ae4fb600f
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-05-14 13:56:27 -0400

    useEffect called to get list of exercises and added function to delete an exercise

--- src/components/ExercisesList.js
+++ src/components/ExercisesList.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function ExercisesList() {
+    const [exercises, setExercises] = useState([])
+
+    useEffect(() => {
+        return (
+            axios.get('http://localhost:5000/exercises/')
+                .then(response => {
+                    setExercises(response.data)
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+        )
+    }, [])
+
+    const deleteExercise = id => {
+        axios.delete('http://localhost:5000/exercises/' + id)
+            .then(res => console.log(res.data));
+        setExercises(exercises.filter(el => el._id !== id))
+    }
+
     return (
         <div>
             <p>You are on the Exercise List component</p>
