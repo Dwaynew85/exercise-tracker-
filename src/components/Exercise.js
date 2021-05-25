aca8d49a9b9a36763e5788310184d7a93148b22a
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-05-24 23:31:34 -0400

    corrected spelling

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -9,7 +9,7 @@ function Exercise({exercise, deleteExercise}) {
          <td>{exercise.duration}</td>   
          <td>{exercise.date.substring(0,10)}</td>
          <td>
-             <Link to={"/edit/"+exercise.id}>edit</Link> | <a href="#" onClick={ () => { deleteExercise(exercise.id) }}>delete</a>
+             <Link to={"/edit/"+exercise._id}>edit</Link> | <a href="#" onClick={() => { deleteExercise(exercise._id) }}>delete</a>
         </td>   
         </tr>
     )
